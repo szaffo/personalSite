fa29fbe5ab5d9ba4e8fe61c2de5ac2c145ab89aa
Szabó Martin <martin77szabo@gmail.com>
2020-05-24 13:28:50 +0200

    stratego game demo added

---
+++ public/stratego/precache-manifest.7bc09652014ddeef12defe4c63f7a9b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c29608ad8bb2a003a98ed233c93d29d",
+    "url": "/stratego/index.html"
+  },
+  {
+    "revision": "c22ae3e627c39d1f5c8c",
+    "url": "/stratego/static/css/main.dc28e11e.chunk.css"
+  },
+  {
+    "revision": "a8fa0dcd22e19111285d",
+    "url": "/stratego/static/js/2.45550f69.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/stratego/static/js/2.45550f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22ae3e627c39d1f5c8c",
+    "url": "/stratego/static/js/main.b546bacf.chunk.js"
+  },
+  {
+    "revision": "6e5ae07f89b2f4dcb378",
+    "url": "/stratego/static/js/runtime-main.675c1eaa.js"
+  }
+]);
